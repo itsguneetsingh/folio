510e72a993f3fe95b110b3e8a88a3c88e5aee592
Guneet Singh <itsguneetsingh@gmail.com>
2023-12-28 18:00:54 +0530

    changed the view

--- src/Components/modal/index.jsx
+++ src/Components/modal/index.jsx
@@ -63,7 +63,7 @@ export default function index({modal, projects, link}) {
             }
             </div>
         </motion.div>
-        <motion.div ref={cursor} className={styles.cursor} variants={scaleAnimation} initial="initial" animate={active ? "enter" : "closed"}></motion.div>
+        {/* <motion.div ref={cursor} className={styles.cursor} variants={scaleAnimation} initial="initial" animate={active ? "enter" : "closed"}></motion.div> */}
         {/* <motion.div onClick={openProject(link)} ref={cursorLabel} className={styles.cursorLabel} variants={scaleAnimation} initial="initial" animate={active ? "enter" : "closed"}>View</motion.div> */}
     </>
   )
